88b991503b691e2640e597034adf2663b337873f
Sandhyachahar <sandhyachahar.cse@gmail.com>
2026-07-19 15:44:18 +0530

    Update frontend API URL for production

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 connectDB();
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 8000 ;
 
 app.listen(PORT, () => {
     console.log(`Listening To port ${PORT}`);
